9669e1d9d6bf006f1a5df586527ca7f262c90d99
jnoconor <joe.oconor@gmail.com>
2013-10-08 13:45:55 -0400

    update scrape-to-db to include actual db schema

--- scrape-to-db.rb
+++ scrape-to-db.rb
@@ -14,9 +14,20 @@ db = create_db
 
 def create_table(database)
   rows = database.execute <<-whatever
-    CREATE TABLE IF NOT EXISTS students (
-      name TEXT,
-      bio TEXT
+    CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    social_links TEXT,
+    quote TEXT,
+    biography TEXT,
+    education TEXT,
+    work TEXT,
+    coder_links TEXT,
+    blogs TEXT,
+    fav_websites TEXT,
+    personal_projects TEXT,
+    flatiron_projects TEXT,
+    fave_cities TEXT
     );
   whatever
 end
